6b29b1182d9d3875cc9555042b9fc112a5156655
ricky <ricky9667@gmail.com>
2019-11-07 17:37:22 +0800

    Finished

--- hw_chap08_108820008/chap08_project06/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-/*****************************************************************/
-/* Class: Computer Programming, Fall 2019                        */
-/* Author: 胡紹宇                                                */
-/* ID: 108820008                                                 */
-/* Date: 2019.10.29                                              */
-/* Purpose: Convert English to B1ff-speak                        */
-/* Change History: log the change history of the program         */
-/*****************************************************************/
--- hw_chap09_108820008/chap09_project01/chap09_project01.c
+++ hw_chap09_108820008/chap09_project01/chap09_project01.c
@@ -13,15 +13,14 @@
 
 void selection_sort(int a[MAX_LEN],int n)
 {
-//	printf("n=%d\n",n);
-	if(n<=1) return;
-	int p=0,i,tmp;
+	if(n<=1) return; // return when sorting is finished
+	int p=0,i,tmp; 
 	for(i=1;i<n;i++)
 		if(a[i]>a[p]) 
-			p=i;
+			p=i; // p saves the largest number of the array
 	
-	tmp=a[p], a[p]=a[n-1], a[n-1]=tmp;
-	return selection_sort(a,n-1);
+	tmp=a[p], a[p]=a[n-1], a[n-1]=tmp; // swap the largest and the last number
+	return selection_sort(a,n-1); // make end -1 and recall the function
 }
 
 int main()
@@ -30,16 +29,16 @@ int main()
 	printf("Enter list of integers to be sorted: ");
 	for(i=0,n=0;i<MAX_LEN;i++)
 	{
-		scanf("%d",&a[i]);
-		n++;
-		if((c=getchar())=='\n')
-			break;
-		ungetc(c,stdin);
+		scanf("%d",&a[i]); // input
+		n++; // count number of integers
+		if((c=getchar())=='\n') 
+			break; // break if you get a '\n'
+		ungetc(c,stdin); // push the character back to stdin
 	}
 
-	selection_sort(a,n);
+	selection_sort(a,n); // sort
 
-	printf("Sorted list: ");
+	printf("Sorted list: "); // output the sorted list
 	for(i=0;i<n;i++)
 		printf("%d ",a[i]);
 	puts("");
