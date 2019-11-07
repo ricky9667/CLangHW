fd1fa7178493fd309853b3558ab200ef75be2505
ricky <ricky9667@gmail.com>
2019-11-07 16:58:39 +0800

    excuse me wtf

--- hw_chap09_108820008/chap09_project01/chap09_project01.c
+++ hw_chap09_108820008/chap09_project01/chap09_project01.c
@@ -9,8 +9,41 @@
 
 #include<stdio.h>
 
+#define MAX_LEN 100
+
+void selection_sort(int* a,int n)
+{
+//	printf("n=%d\n",n);
+	if(n<=1) return;
+	int p,i,tmp;
+
+	for(i=1;i<n;i++)
+		if(a[i]>a[p]) 
+			p=i;
+	
+	tmp=a[p], a[p]=a[n-1], a[n-1]=tmp;
+	return selection_sort(a,n-1);
+}
+
 int main()
 {
+	int i,c,n,a[MAX_LEN];
+	printf("Enter list of integers to be sorted: ");
+	for(i=0,n=0;i<MAX_LEN;i++)
+	{
+		scanf("%d",&a[i]);
+		n++;
+		if((c=getchar())=='\n')
+			break;
+		ungetc(c,stdin);
+	}
+
+	selection_sort(a,n);
+
+	printf("Sorted list: ");
+	for(i=0;i<n;i++)
+		printf("%d ",a[i]);
+	puts("");
 
 	return 0;
 }
