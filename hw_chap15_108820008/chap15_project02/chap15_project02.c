d2b79cba3c6ed9ad6ca6dd8928dd18075d5b69eb
Ricky Hu <ricky9667@gmail.com>
2019-12-23 14:34:55 +0800

    15-2

--- hw_chap15_108820008/chap15_project02/chap15_project02.c
+++ hw_chap15_108820008/chap15_project02/chap15_project02.c
@@ -1,7 +1,14 @@
+/*****************************************************************/
+/* Class: Computer Programming, Fall 2019                        */
+/* Author: 胡紹宇                                                */
+/* ID: 108820008                                                 */
+/* Date: 2019.12.23                                              */
+/* Purpose:                                                      */
+/* Change History: log the change history of the program         */
+/*****************************************************************/
 #include <string.h>
 #include "line.h"
 #include "word.h"
-
 #define MAX_WORD_LEN 20
 
 int main(int argc, char *argv[]) 
@@ -12,20 +19,20 @@ int main(int argc, char *argv[])
 
     for(;;)
     {
-        read_word(word, MAX_WORD_LEN + 1);
-        word_len = strlen(word);
+        read_word(word, MAX_WORD_LEN + 1); // input word
+        word_len = strlen(word); // get string length
         if (word_len == 0) {
-            flush_line();
+            flush_line(); // exit
             return 0;
         }
-        if (word_len > MAX_WORD_LEN)
-            word[MAX_WORD_LEN] = '*';
+        // if (word_len > MAX_WORD_LEN)
+        //     word[MAX_WORD_LEN] = '*';
         if (word_len+1 > space_remaining()) 
         {
-            write_line();
-            clear_line();
+            write_line(); // put word to string
+            clear_line(); // clear word
         }
-        add_word(word);
+        add_word(word); // put the word to string array
     }
     return 0;
 }
--- hw_chap15_108820008/chap15_project02/word.c
+++ hw_chap15_108820008/chap15_project02/word.c
@@ -9,13 +9,16 @@ int read_char(void)
     return ch;
 }
 
-void read_word(char *word, int len) 
+void read_word(char *word, int len)
 {
+    const int max_len = len-1;
     int ch, pos = 0;
     while ((ch = read_char()) == ' ');
     while (ch != ' ' && ch != EOF) {
         if (pos < len)
             word[pos++] = ch;
+        if(pos > max_len)
+            word[max_len] = '*';
         ch = read_char();
     }
     word[pos] = '\0';
